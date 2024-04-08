1ca5896e57771597b42c51a301a21bbc345cd4cc
jgranados4 <jgranados@est.ups.edu.ec>
2024-04-08 16:54:26 -0500

    feat:serilog.asp .net

--- Program.cs
+++ Program.cs
@@ -8,15 +8,17 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 //Logger
-var logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger();
-builder.Logging.AddSerilog(logger);
-// Add services to the container.
+Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger();
+builder.Host.UseSerilog();
+//builder.Host.UseSerilog((context,configuration)=>configuration.ReadFrom.Configuration(context.Configuration));
+//builder.Logging.AddSerilog(logger);
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//Conexion a la base de datos
 builder.Services.AddDbContext<HolamundoContext>(options =>
  options.UseMySql(builder.Configuration.GetConnectionString("conexion"), Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql")));
 //cors
@@ -27,7 +29,9 @@ builder.Services.AddCors(options =>
 //Autentificacion
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
-    {
+    {   
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer=true,
@@ -41,6 +45,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 //services
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IUtilidadesService,UtilidadesService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -49,7 +54,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 
 app.UseCors("CorsPolicy");
